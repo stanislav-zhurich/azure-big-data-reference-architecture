ae0571d9ece122d6dc8eb4ba5564d582117dd99d
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-08-02 00:11:04 +0300

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,21 @@
 data "azuread_client_config" "current" {}
 
 locals {
-  resource_prefix = "project-${data.azuread_client_config.current.object_id}"
+  resource_prefix = "bigdata${random_string.random_prefix.id}"
+}
+
+resource "random_string" "random_prefix" {
+  length  = 10
+  special = false
+  upper   = false
+}
+
+resource "time_sleep" "role_assignment_sleep" {
+  create_duration = "60s"
+
+  triggers = {
+    role_assignment = azurerm_role_assignment.user_datalake_role_assignement["Contributor"].id
+  }
 }
 
 resource "azurerm_resource_group" "resource_group" {
@@ -10,7 +24,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_storage_account" "datalake_storage_account" {
-  name                     = substr(replace("dl${local.resource_prefix}", "-", ""), 0, 24)
+  name                     = substr("datalake${local.resource_prefix}", 0, 24)
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
@@ -21,7 +35,7 @@ resource "azurerm_storage_account" "datalake_storage_account" {
 }
 
 resource "azurerm_storage_account" "source_storage_account" {
-  name                     = substr(replace("src${local.resource_prefix}", "-", ""), 0, 24)
+  name                     = substr("source${local.resource_prefix}", 0, 24)
   resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = "Standard"
@@ -37,16 +51,8 @@ resource "azurerm_storage_container" "source_container" {
 }
   
 
-resource "time_sleep" "role_assignment_sleep" {
-  create_duration = "60s"
-
-  triggers = {
-    role_assignment = azurerm_role_assignment.user_datalake_role_assignement["Contributor"].id
-  }
-}
-
 resource "azurerm_data_factory" "data_factory" {
-  name                = substr("df-${local.resource_prefix}", 0, 63)
+  name                = substr("data-factory-${local.resource_prefix}", 0, 63)
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   github_configuration {
@@ -89,6 +95,11 @@ resource "azurerm_role_assignment" "df_source_role_assignement" {
 
 resource "azurerm_storage_data_lake_gen2_filesystem" "adls" {
   name               = "datalake"
+  ace {
+    id       = data.azuread_client_config.current.object_id
+    type     = "user"
+    permissions = "rwx"
+  }
   storage_account_id = azurerm_storage_account.datalake_storage_account.id
   depends_on = [ time_sleep.role_assignment_sleep ]
 }
@@ -108,6 +119,11 @@ resource "azurerm_storage_data_lake_gen2_path" "datalake_path" {
   }
   ace {
     id       = data.azuread_client_config.current.object_id
+    type     = "user"
+    permissions = "rwx"
+  }
+  ace {
+    id       = azuread_service_principal.aad_service_sp.application_id
     type     = "user"
     permissions = "rwx"
   }
@@ -144,12 +160,12 @@ resource "azurerm_data_factory_linked_service_key_vault" "key_vault_linked_servi
 
 
 resource "azurerm_key_vault" "key_vault" {
-  name                     = "bigdata-project-kv"
+  name                     = "${local.resource_prefix}-kv"
   location                 = azurerm_resource_group.resource_group.location
   resource_group_name      = azurerm_resource_group.resource_group.name
   tenant_id                = data.azuread_client_config.current.tenant_id
   sku_name                 = "standard"
-  purge_protection_enabled = true
+  purge_protection_enabled = false
 } 
 
 resource "azurerm_key_vault_access_policy" "deployer_keyvault_policy" {
@@ -157,7 +173,7 @@ resource "azurerm_key_vault_access_policy" "deployer_keyvault_policy" {
    tenant_id    = data.azuread_client_config.current.tenant_id
    object_id    = data.azuread_client_config.current.object_id
    secret_permissions = [
-     "Get", "List", "Set", "Delete", "Recover"
+     "Get", "List", "Set", "Delete", "Recover", "Purge"
    ]
 }
 resource "azurerm_key_vault_access_policy" "df_keyvault_policy" {
@@ -166,7 +182,17 @@ resource "azurerm_key_vault_access_policy" "df_keyvault_policy" {
    object_id    = azurerm_data_factory.data_factory.identity[0].principal_id
 
    secret_permissions = [
-     "Get", "List", "Set", "Delete", "Recover"
+     "Get", "List", "Set", "Delete", "Recover", "Purge"
+   ]
+ }
+
+ resource "azurerm_key_vault_access_policy" "adb_keyvault_policy" {
+   key_vault_id = azurerm_key_vault.key_vault.id
+   tenant_id    = data.azuread_client_config.current.tenant_id
+   object_id    = azuread_service_principal.aad_service_sp.object_id
+
+   secret_permissions = [
+     "Get", "List", "Set", "Delete", "Recover", "Purge"
    ]
  }
 
@@ -193,7 +219,7 @@ resource "azurerm_key_vault_secret" "datalake-access-key-secret" {
 
 
 resource "azurerm_databricks_workspace" "databricks_workspace" {
-  name                = substr("databricks-${local.resource_prefix}", 0, 63)
+  name                = substr("databricks-workspace-${local.resource_prefix}", 0, 63)
   resource_group_name = azurerm_resource_group.resource_group.name
   location            = azurerm_resource_group.resource_group.location
   sku                 = "trial"
@@ -203,23 +229,9 @@ resource "azurerm_databricks_workspace" "databricks_workspace" {
   }
 }
 
-provider "databricks" {
-  host = azurerm_databricks_workspace.databricks_workspace.workspace_url
-}
-
-/* data "databricks_node_type" "smallest" {
-  local_disk = true
-}
-
-# Use the latest Databricks Runtime
-# Long Term Support (LTS) version.
-data "databricks_spark_version" "latest_lts" {
-  long_term_support = true
-  depends_on = [ azurerm_databricks_workspace.databricks_workspace ]
-} */
 
 resource "databricks_cluster" "databricks_cluster" {
-  cluster_name            = substr("cluster-${local.resource_prefix}", 0, 63)
+  cluster_name            = substr("databricks-cluster-${local.resource_prefix}", 0, 63)
   node_type_id            = "Standard_F4"
   spark_version           = "12.2.x-scala2.12"
   autotermination_minutes = 15
@@ -247,32 +259,100 @@ resource "azurerm_role_assignment" "df_databricks_role_assignement" {
   principal_id         = azurerm_data_factory.data_factory.identity[0].principal_id  
 }  
 
-data "databricks_current_user" "current" {}
+resource "azurerm_role_assignment" "deployer_databricks_role_assignement" {  
+  for_each = toset(["Contributor", "Storage Blob Data Owner"])
+  role_definition_name               = each.value
+
+  scope                = azurerm_databricks_workspace.databricks_workspace.id   
+  principal_id         = data.azuread_client_config.current.object_id  
+}  
+
+resource "time_sleep" "databricks_role_assignment_sleep" {
+  create_duration = "60s"
+
+  triggers = {
+    role_assignment = azurerm_role_assignment.deployer_databricks_role_assignement["Contributor"].id
+  }
+}
+
+data "databricks_current_user" "current" {
+  depends_on = [ azurerm_role_assignment.deployer_databricks_role_assignement ]
+}
 
 resource "databricks_notebook" "notebook" {
   path     = "${data.databricks_current_user.current.home}/pipeline/copyFromBronzeToSilver.py"
   language = "PYTHON"
-  source   = "../copyFromBronzeToSilver.py"
+  source   = "../databricks/src/copyFromBronzeToSilver.py"
 }
 
-resource "databricks_secret_scope" "terraform" {
-    name                     = "application"
-    initial_manage_principal = "users"
+resource "azuread_application" "databricks_service_application" {
+  display_name = "databricks_service_application"
+  identifier_uris  = ["api://databricks_service_application"]
+  owners       = [data.azuread_client_config.current.object_id]
 }
 
-resource "databricks_secret" "service_principal_key" {
-    key          = "service_principal_key"
-    string_value = "${var.ARM_CLIENT_SECRET}"
-    scope        = databricks_secret_scope.terraform.name
+resource "azuread_service_principal" "aad_service_sp" {
+  application_id               = azuread_application.databricks_service_application.application_id
+  app_role_assignment_required = false
+  owners                       = [data.azuread_client_config.current.object_id]
 }
 
-resource "databricks_azure_adls_gen2_mount" "datalake_mount" {
-    container_name         = "datalake"
-    storage_account_name   = azurerm_storage_account.datalake_storage_account.name
-    mount_name             = "datalake"
-    tenant_id              = data.azuread_client_config.current.tenant_id
-    client_id              = data.azuread_client_config.current.client_id
-    client_secret_scope    = databricks_secret_scope.terraform.name
-    client_secret_key      = databricks_secret.service_principal_key.key
-    initialize_file_system = true
+resource "azuread_service_principal_password" "service_sp_pswd" {
+  service_principal_id = azuread_service_principal.aad_service_sp.object_id
 }
+
+
+resource "azurerm_role_assignment" "databricks_dl_role_assignment" {  
+  for_each = toset(["Contributor", "Storage Blob Data Owner"])
+  role_definition_name               = each.value
+
+  scope                = azurerm_storage_account.datalake_storage_account.id   
+  principal_id         = azuread_service_principal.aad_service_sp.object_id 
+}
+
+resource "azurerm_key_vault_secret" "adb_client_id" {
+   depends_on = [ azurerm_key_vault_access_policy.deployer_keyvault_policy]
+   name         = "client-id"
+   value = azuread_service_principal.aad_service_sp.application_id
+   key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "adb_client_secret" {
+   depends_on = [ azurerm_key_vault_access_policy.deployer_keyvault_policy]
+   name         = "client-secret"
+   value = azuread_service_principal_password.service_sp_pswd.value
+   key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "adb_tenant_id" {
+   depends_on = [ azurerm_key_vault_access_policy.deployer_keyvault_policy]
+   name         = "client-tenant-id"
+   value = data.azuread_client_config.current.tenant_id
+   key_vault_id = azurerm_key_vault.key_vault.id
+}
+
+resource "databricks_secret_scope" "kv" {
+  name = "keyvault-managed"
+  depends_on = [ databricks_cluster.databricks_cluster, azurerm_key_vault.key_vault ]
+
+  keyvault_metadata {
+    resource_id = azurerm_key_vault.key_vault.id
+    dns_name    = azurerm_key_vault.key_vault.vault_uri
+  }
+}
+
+ resource "databricks_mount" "databricks_mound" {
+  depends_on = [ databricks_cluster.databricks_cluster, azurerm_key_vault.key_vault ]
+  name = "datalake_mount"
+  cluster_id = databricks_cluster.databricks_cluster.cluster_id
+  uri = "abfss://${azurerm_storage_data_lake_gen2_filesystem.adls.name}@${azurerm_storage_account.datalake_storage_account.name}.dfs.core.windows.net"
+  extra_configs = {
+    "fs.azure.account.auth.type" : "OAuth",
+    #"fs.azure.account.auth.type.${azurerm_storage_account.datalake_storage_account.name}.dfs.core.windows.net" : "OAuth",
+    "fs.azure.account.oauth.provider.type" : "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
+    "fs.azure.account.oauth2.client.id" : azuread_service_principal.aad_service_sp.application_id
+    "fs.azure.account.oauth2.client.secret" : "{{secrets/keyvault-managed/client-secret}}",
+    "fs.azure.account.oauth2.client.endpoint" : "https://login.microsoftonline.com/${data.azuread_client_config.current.tenant_id}/oauth2/token",
+    "fs.azure.createRemoteFileSystemDuringInitialization" : "false",
+  }
+} 
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,6 +4,13 @@ provider "azurerm" {
   }
 }
 
+provider "databricks" {
+  azure_workspace_resource_id = azurerm_databricks_workspace.databricks_workspace.id
+  host = azurerm_databricks_workspace.databricks_workspace.workspace_url
+  #azure_use_msi = true
+  #auth_type  = "azure-cli"
+}
+
 /* provider "azuread" {
   client_id       = var.client_id
   client_secret   = var.client_secret
