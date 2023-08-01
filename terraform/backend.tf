510a2745d57f486b28a56ae2862f445b33d7c7d2
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-08-02 00:10:52 +0300

    update terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,10 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.37.0"
+      version = "=3.67.0"
     }
     databricks = {
       source = "databricks/databricks"
+    }
+    
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
     }
   }
 }
