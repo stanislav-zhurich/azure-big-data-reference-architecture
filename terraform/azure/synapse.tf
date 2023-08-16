341db4567e7cfb2d2d2848a9ef4449745cd58d69
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-08-16 21:28:56 +0300

    add synapse

---
+++ terraform/azure/synapse.tf
@@ -0,0 +1,14 @@
+resource "azurerm_synapse_workspace" "synapse_workspace" {
+  name                                 = substr("synapse-workspace-${local.resource_prefix}", 0, 63)
+  resource_group_name                  = azurerm_resource_group.resource_group.name
+  location                             = azurerm_resource_group.resource_group.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_data_lake_gen2_filesystem.adls.id
+  sql_administrator_login              = "sqladminuser"
+  sql_administrator_login_password     = "H@Sh1CoR3!"
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = local.tags
+}
