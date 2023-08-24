ad587af850acc36edd062b7e6c3eb23b66ba9069
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-08-24 10:02:20 +0300

    output and infra image

--- terraform/azure/output.tf
+++ terraform/azure/output.tf
@@ -1,3 +1,14 @@
-output "databricks_url" {
-  value = azurerm_databricks_workspace.databricks_workspace.workspace_url
+output "databricks_studio_url" {
+  value = "https://${azurerm_databricks_workspace.databricks_workspace.workspace_url}"
+  description = "Databricks studio web endpoint"
+}
+
+output "synapse_studio_url" {
+  value = azurerm_synapse_workspace.synapse_workspace.connectivity_endpoints.web
+  description = "Synapse studio web endpoint"
+}
+
+output "datafactory_studio_url" {
+  value = "https://adf.azure.com/en/home"
+  description = "Data Factory studio web endpoint"
 }
