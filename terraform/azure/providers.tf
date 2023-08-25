0c7b5f45883d72c9ca352e431b65fafb7dd82634
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-08-25 11:22:56 +0300

    remove synapse old tf

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -8,11 +8,4 @@ provider "azurerm" {
 
 provider "databricks" {
   azure_workspace_resource_id = azurerm_databricks_workspace.databricks_workspace.id
-  //auth_type  = "azure-cli"
 }
-
-/* provider "azuread" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-} */
